fee2abde872afda216a41bf4f3359921fb55dfa7
hanzlamateen <hanzlamateen@live.com>
2023-06-18 11:45:32 +0300

    Changes for coil

--- feathers-chat-ts/src/services/index.ts
+++ feathers-chat-ts/src/services/index.ts
@@ -1,10 +1,12 @@
 import { message } from './messages/messages'
 import { user } from './users/users'
+import { coilSetting } from './coil-setting/coil-setting'
 // For more information about this file see https://dove.feathersjs.com/guides/cli/application.html#configure-functions
 import type { Application } from '../declarations'
 
 export const services = (app: Application) => {
   app.configure(message)
   app.configure(user)
+  app.configure(coilSetting)
   // All services will be registered here
 }
